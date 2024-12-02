4d8fbf79d5192c9df086b3e995d9d13b7418170c
EpicMo <epicmocn@gmail.com>
2024-12-02 20:13:07 +0800

    [+] 加入导航路由

--- ZeroPlay/App.xaml.cs
+++ ZeroPlay/App.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -15,6 +17,8 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using ZeroPlay.View;
+using ZeroPlay.ViewModel;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +30,10 @@ namespace ZeroPlay
     /// </summary>
     public partial class App : Application
     {
+        public IHost Host { get; private set; }
+
+        public static T? GetRequiredService<T>() where T : class => (Application.Current as App)?.Host.Services.GetService<T>();
+
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
@@ -33,6 +41,21 @@ namespace ZeroPlay
         public App()
         {
             this.InitializeComponent();
+
+            // Configure Host for Dependency Injection
+            Host = Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder()
+                   .UseContentRoot(AppContext.BaseDirectory)
+                   .ConfigureServices((ctx, services) =>
+                   {
+                       // Page
+                       services.AddSingleton<HomePage, HomePage>();
+                       services.AddSingleton<SettingsPage, SettingsPage>();
+                       services.AddSingleton<ProfilePage, ProfilePage>();
+
+                       // ViewModel
+                       services.AddSingleton<HomeViewModel, HomeViewModel>();
+                   })
+                   .Build();
         }
 
         /// <summary>
--- ZeroPlay/MainWindow.xaml.cs
+++ ZeroPlay/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ApplicationSettings;
+using ZeroPlay.View;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,11 +28,36 @@ namespace ZeroPlay
         public MainWindow()
         {
             this.InitializeComponent();
+
+            // 使用自己的 Title Bar
+            ExtendsContentIntoTitleBar = true;
+            SetTitleBar(AppTitleBar);
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private void NavigateController_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
         {
-            myButton.Content = "Clicked";
+            // 判断预先设置的页面
+            if (NavigateController.SelectedItem == NavigateController.SettingsItem)
+            {
+                NavigationContentFrame.Content = App.GetRequiredService<SettingsPage>();
+            }
+
+            // 接下来判断其他的自定义页面
+            if (NavigateController.SelectedItem is NavigationViewItem selectedItem)
+            {
+                string pageTag = selectedItem.Tag.ToString()!;
+                switch (pageTag)
+                {
+                    case "HomePage":
+                        NavigationContentFrame.Content = App.GetRequiredService<HomePage>();
+                        break;
+                    case "ProfilePage":
+                        NavigationContentFrame.Content = App.GetRequiredService<ProfilePage>();
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
