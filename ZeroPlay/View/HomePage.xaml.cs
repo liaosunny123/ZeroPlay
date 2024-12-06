dc8faafba4bc716bc011f1e5e82eda574f9352dc
forDecember <1921567337@qq.com>
2024-12-07 01:18:08 +0800

    update: HomePage媒体展示框架

--- ZeroPlay/View/HomePage.xaml.cs
+++ ZeroPlay/View/HomePage.xaml.cs
@@ -12,6 +12,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using ZeroPlay.ViewModel;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,9 +24,28 @@ namespace ZeroPlay.View
     /// </summary>
     public sealed partial class HomePage : Page
     {
+
+        private HomeViewModel _viewModel;
+
         public HomePage()
         {
             this.InitializeComponent();
+
+            // 创建视图模型实例
+            _viewModel = new HomeViewModel();
+
+            // 设置页面的数据上下文为视图模型，方便进行数据绑定
+            this.DataContext = _viewModel;
+
+            // 使用BindingOperations.SetBinding来设置FlipView的ItemsSource绑定
+            BindingOperations.SetBinding(flipViewVertical, FlipView.ItemsSourceProperty, new Binding
+            {
+                Path = new PropertyPath("MediaItems"),
+                Source = _viewModel
+            });
         }
     }
+
+    // 定义一个简单的数据项类，用于存储每个FlipView展示项对应的图像和名称信息
+
 }
--- ZeroPlay/ViewModel/HomeViewModel.cs
+++ ZeroPlay/ViewModel/HomeViewModel.cs
@@ -1,13 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ZeroPlay.ViewModel
 {
-    public class HomeViewModel
+    public class HomeViewModel : INotifyPropertyChanged
     {
+        private ObservableCollection<MediaItem> _mediaItems;
+        public ObservableCollection<MediaItem> MediaItems
+        {
+            get { return _mediaItems; }
+            set
+            {
+                _mediaItems = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public HomeViewModel()
+        {
+            // 初始化数据集合并添加示例数据
+            MediaItems = new ObservableCollection<MediaItem>();
+            MediaItems.Add(new MediaItem { Image = "Assets/Image1.jpg", Name = "First Media" });
+            MediaItems.Add(new MediaItem { Image = "Assets/Image2.jpg", Name = "Second Media" });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public class MediaItem
+        {
+            public string Image { get; set; }
+            public string Name { get; set; }
+        }
 
     }
 }
